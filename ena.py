a352c051a0587da680a17f98b2e6671bca7db745
Dustan Bower <dustan.bower@gmail.com>
2014-10-11 11:00:40 -0400

    add additional tests and functionality for user handling
    extras and such that will be only partially imported into master
    
    remove caching information which was being difficult
    
    continue removing overcomplicated cache code
    
    update code to latest form

--- ena.py
+++ ena.py
@@ -1,59 +1,117 @@
 #!/usr/bin/env python
-from datetime import datetime
 
+"""
+Module/Script for parsing sign-in data from google drive and determining
+Ascendancy and Eminence.
+
+"""
+
+from collections import defaultdict
+from datetime import datetime
+import operator
+
+import argparse
 import config
-import ena
 import gspread
 
 
-gdrive = gspread.login(config.USERNAME, config.PASSWORD)
-#for worksheet in gdrive.open(config.FILENAME).worksheets()
-#    worksheet.sheet1.col_values(rows.index('Allegiance') + 1)
+VERBOSE = False
+STFU = False
 
-#dict = {}
-#for worksheet in gdrive.open(config.FILENAME).worksheets()
-#    lines = worksheet.get_all_values()
-#    headers = lines.pop(0)
-#    for line in lines:
-#        for num, value in enumerate(line):
-#            dict[headers[num]] = value
+# lookup fields for rows by name
+DATA_FIELDS = {
+    "clan": config.CLAN_FIELD,
+    "covenant": config.COVENANT_FIELD,
+    "count?": config.COUNT_FIELD,
+    "player_name": config.PLAYER_NAME,
+    "mship_number": config.MEMBERSHIP_NUMBER,
+    "status": config.STATUS_FIELD,
+}
 
-
-# maybe make a list of dictionaries
-#data = []
-#for worksheet in gdrive.open(config.FILENAME).worksheets()
-#    lines = worksheet.get_all_values()
-#    headers = lines.pop(0)
-#    for line in lines:
-#        dict = {}
-#        for num, value in enumerate(line):
-#            dict[headers[num]] = value
-#        data.append(dict)
 
 def login(username, password):
-    return gspread.login(username, password)
+    """
+    Log-in to Google Spreadsheets and return a client object.
+
+    username: The username to sign into google docs.
+    password: The password for google docs.
+
+    """
+
+    client = gspread.client.Client(auth=(username, password))
+    client.login()
+    return client
 
 
-def spreadsheet(login, filename):
-    return login.open(filename)
+def spreadsheet(client, filename):
+    """
+    Open and return the spreadsheet of the specified name.
+
+    client: Google drive client
+    filename: name of file to open
+
+    """
+    return client.open(filename)
 
 
-def filter(data, month=None):
+def filter_list(data, arguments=None):
     """
     Filter a list of dictionaries so the specified value it matches specified
             terms.  (for now we just filter by months)
 
     data: the list of dictionaries to consider
 
-    month: Specify a particular month to filter against.
+    arguments: Specify fields to filter against:
+        month: month (int 1 to 12)
+        year: year (after 2000.  13 is considered abbreviation for 2013)
+
+        clan: Name of clan
+        covenant: Name of covenant
+        count?: Whether this entry should be counted for E&A
 
     """
 
-    if month:
-        # Only copy the data that matches the specified month to the new list
+    if arguments == None:
+        arguments = {}
+
+    filter_by = defaultdict(lambda: None)
+
+    for k, v in arguments.iteritems():
+        filter_by[k] = v
+
+    special_keys = ['month', 'year']
+    special_fields = {}
+
+    # special keys need to be treated differently than regular filters and
+    # removed before we handle default filtering
+    for field in special_keys:
+        special_fields[field] = filter_by[field]
+        del filter_by[field]
+
+        if field == 'year':
+            year = special_fields['year']
+            # strptime gives us the full year, so we need to correct
+            # 13 to 2013, etc.
+            special_fields['year'] = (year + 2000
+                                      if year is not None and year < 2000
+                                      else year)
+
+    for date_field in ('month', 'year'):
+        # Only copy the data that matches the specified field to the new list
+
+        if special_fields[date_field]:
+            data = [_ for _ in data
+                    if getattr(datetime.strptime(_[config.DATE_FIELD],
+                                   config.DATE_FORMAT),
+                               date_field) == special_fields[date_field]]
+
+    for filter_key, filter_value in filter_by.iteritems():
+        # update the specified key to match the lookup table used by the
+        # data from the spreadsheet
+        filter_key = DATA_FIELDS[filter_key]
+
         data = [_ for _ in data
-                if datetime.strptime(_[config.DATE_FIELD],
-                                     config.DATE_FORMAT).month == month]
+                if _[filter_key].lower() == filter_value.lower()]
 
     return data
 
@@ -66,19 +124,174 @@ def data_to_dict(spreadsheet_data):
         ['3', '0']]  becomes [{'lives': '1', 'score': '99'}, {'lives': '1',
             score: '99'}]
 
+    spreadsheet_data: list of lists, where the first item is a list of headers
+                    to consider fields of.
     """
+
+    spreadsheet_data = spreadsheet_data[:] # copy data so we don't alter it
 
     # get our new key values from the first entry in the list of lists
     headers = spreadsheet_data.pop(0)
 
-    data = [] 
+    data = []
 
     # go line by line and convert everything
     for line in spreadsheet_data:
-        dict = {}
+        dict_ = {}
         for num, value in enumerate(line):
-            dict[headers[num]] = value
+            dict_[headers[num]] = value
 
-        data.append(dict)
+        data.append(dict_)
 
     return data
+
+
+def parse_date(date_string):
+    """ Parse a date in the form 1/12 into a dict specifying month/year. """
+
+    month, year = date_string.split('/')
+    return {'month': int(month), 'year': int(year)}
+
+
+def calculate_status(data):
+    """
+    Calculate total status of each Covenant and Clan and return a dictionary.
+
+    data: spreadsheet data in dictionary form
+
+    """
+    members = {}
+    statuses = {}
+
+    TYPES = ['clan', 'covenant']
+    for type_ in TYPES:
+        members[type_] = defaultdict(int)
+        statuses[type_] = defaultdict(int)
+
+    seen = {}
+    seen['mship_numbers'] = []
+    seen['players'] = []
+    seen['data'] = []
+
+    excluded = []
+
+    for entry in data:
+        mship_number = entry[DATA_FIELDS['mship_number']].upper()
+        player_name = entry[DATA_FIELDS['player_name']].lower()
+
+        if (mship_number in seen['mship_numbers'] or
+                player_name in seen['players']):
+
+            for seen_mship, seen_name in seen['data']:
+                mship_match = name_match = False
+
+                if seen_mship == mship_number:
+                    if VERBOSE:
+                        print('match membership')
+                    mship_match = True
+
+                if seen_name == player_name:
+                    if VERBOSE:
+                        print('match name')
+                    name_match = True
+
+                if not STFU and (mship_match or name_match):
+                    print('{} ({}) collides with {} ({}), excluding'.format(
+                        player_name.title(), mship_number, seen_name.title(),
+                        seen_mship))
+
+                    excluded.append({'mship_number': mship_number,
+                                     'player_name': player_name,
+                                     'collides with': (seen_name, seen_mship)})
+                    break
+
+            if not STFU and not (seen_mship or seen_name):
+                print('{} ({}) was excluded for unknown reasons'.format(
+                    mship_number, player_name.title()))
+
+            continue
+
+        player = entry[DATA_FIELDS['player_name']].lower()
+
+        # Let's not treat empty mship numbers as matches, or names for that
+        # matter.  Empty mship numbers aren't tracked, empty names are forced
+        # to be spaces so we can see them on the counted player output and
+        # in any collisions that occur.
+        if mship_number:
+            seen['mship_numbers'].append(mship_number)
+
+        player = ' ' if not player else player
+        if player:
+            seen['players'].append(player)
+        seen['data'].append((mship_number, player))
+
+        for type_ in TYPES:
+            type_name = entry[DATA_FIELDS[type_]].lower().strip()
+            members[type_][type_name] += 1
+
+            pc_status = entry[DATA_FIELDS["status"]]
+            pc_status = (0 if pc_status in config.UNKNOWN_STATUSES
+                         else pc_status)
+            statuses[type_][type_name] += int(pc_status)
+
+
+    if not STFU:
+        print('Counted status for the following UNIQUE players:')
+        if len(seen['players']) == 0:
+            print('    None')
+
+        for player in sorted(seen['players']):
+            print('    ' + player.title())
+
+
+    # convert default dicts to regular dicts and return a list
+    return [dict(_) for _ in members, statuses]
+
+
+def show_results(results):
+    """
+    Sort and display E&A results.
+
+    results: list containing dictionaries, [members, statuses], where members
+            is a dictionary of member numbers with clan/covenant as the key,
+            and statuses is the same, but with status totals.
+
+            I.e., if the only person who signed in was a status 2 Mekhet,
+            results would look like: [{'mekhet': 1}, {'mekhet': 2}]
+    """
+
+    members, statuses = results
+    for type_ in ['clan', 'covenant']:
+        print('')
+        print('{0: <19}  total status  # of members'.format(type_.title()))
+        for entry, status in sorted(statuses[type_].items(),
+                            key=operator.itemgetter(1), reverse=True):
+            print('    {0: <19}: {1: <14} {2}'.format(entry.title(), status,
+                                             members[type_][entry]))
+
+        print('')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("date", help="month/year in the format 12/14, etc.")
+    parser.add_argument("--verbose", help="increase output verbosity",
+                        action="store_true")
+    parser.add_argument("--stfu", help="only print totals, overrides verbose",
+                        action="store_true")
+    args = parser.parse_args()
+
+    STFU = args.stfu
+    VERBOSE = args.verbose if STFU == False else False
+
+    gdrive = login(config.USERNAME, config.PASSWORD)
+    sheet = spreadsheet(gdrive, config.FILENAME)
+    sheet_data = data_to_dict(sheet.sheet1.get_all_values())
+
+    args = parse_date(args.date)
+    # We only care about PCs who are supposed to be counted
+    args['count?'] = 'yes'
+    ena_data = filter_list(sheet_data, args)
+
+    show_results(calculate_status(ena_data))
+    # FIXME: Write unit tests for number tally
--- tests.py
+++ tests.py
@@ -6,13 +6,12 @@ import ena
 
 class LoginTest(unittest.TestCase):
     def test_login(self):
-        gdrive = ena.login(config.USERNAME, config.PASSWORD)
-
+        _ = ena.login(config.USERNAME, config.PASSWORD)
 
 class Spreadsheet(unittest.TestCase):
     def test_spreadsheet(self):
         gdrive = ena.login(config.USERNAME, config.PASSWORD)
-        sheet = ena.spreadsheet(gdrive, config.FILENAME)
+        _ = ena.spreadsheet(gdrive, config.FILENAME)
 
 
 class TestDataToDict(unittest.TestCase):
@@ -23,11 +22,11 @@ class TestDataToDict(unittest.TestCase):
              'Which PC will you be portraying today?',
              'Clan / Character Type', 'Covenant', 'Cadence',
              'What is your City Status (Locals Only)',
-             'This is the PC I want counted for purposes'
+             'This is the PC I want counted for purposes '
              'of Ascendancy/Eminence'],
             ['7/8/14 14:43', 'Nintendo', 'MUSHROOMKINGDOM4', 'Bonzai Bill',
              'Bullet', 'Bowserite', '1', '0 - None or Non Local', 'Yes'],
-            ['9/19/14 12:43', 'Nintendo', 'Hyrule9', 'Zelda',
+            ['9/19/14 12:43', 'Nintendo', 'HYRULE9', 'Zelda',
              'Princess', 'Hyrulean', '5', '5', 'Yes']
         ]
 
@@ -40,40 +39,40 @@ class TestDataToDict(unittest.TestCase):
              'Covenant': 'Bowserite',
              'Cadence': '1',
              'What is your City Status (Locals Only)': '0 - None or Non Local',
-             'This is the PC I want counted for purposes'
+             'This is the PC I want counted for purposes '
              'of Ascendancy/Eminence': 'Yes'},
             {'Timestamp': '9/19/14 12:43',
              'What is your name?': 'Nintendo',
-             'What is your Membership Number?': 'Hyrule9',
+             'What is your Membership Number?': 'HYRULE9',
              'Which PC will you be portraying today?': 'Zelda',
              'Clan / Character Type': 'Princess',
              'Covenant': 'Hyrulean',
              'Cadence': '5',
              'What is your City Status (Locals Only)': '5',
-             'This is the PC I want counted for purposes'
+             'This is the PC I want counted for purposes '
              'of Ascendancy/Eminence': 'Yes'}
         ]
 
         self.assertEqual(ena.data_to_dict(data), result)
 
 
-class TestDateFilter(unittest.TestCase):
-    def test_date_filter(self):
+class TestFilter(unittest.TestCase):
+    def test_filter(self):
         data = [
             ['Timestamp', 'What is your name?',
              'What is your Membership Number?',
              'Which PC will you be portraying today?',
              'Clan / Character Type', 'Covenant', 'Cadence',
              'What is your City Status (Locals Only)',
-             'This is the PC I want counted for purposes'
+             'This is the PC I want counted for purposes '
              'of Ascendancy/Eminence'],
-            ['7/8/14 14:43', 'Nintendo', 'MUSHROOMKINGDOM4', 'Bonzai Bill',
-             'Bullet', 'Bowserite', '1', '0 - None or Non Local', 'Yes'],
-            ['9/19/14 12:43', 'Nintendo', 'Hyrule9', 'Zelda',
+            ['7/8/13 14:43', 'Nintendo', 'MUSHROOMKINGDOM4', 'Bonzai Bill',
+             'Bullet', 'Bowserite', '1', '0 - None or Non Local', 'No'],
+            ['9/19/14 12:43', 'Nintendo', 'HYRULE9', 'Zelda',
              'Princess', 'Hyrulean', '5', '5', 'Yes']
         ]
 
-        month7 = {'Timestamp': '7/8/14 14:43',
+        set1 = {'Timestamp': '7/8/13 14:43',
                   'What is your name?': 'Nintendo',
                   'What is your Membership Number?': 'MUSHROOMKINGDOM4',
                   'Which PC will you be portraying today?': 'Bonzai Bill',
@@ -82,26 +81,90 @@ class TestDateFilter(unittest.TestCase):
                   'Cadence': '1',
                   'What is your City Status (Locals Only)':
                         '0 - None or Non Local',
-                  'This is the PC I want counted for purposes'
-                  'of Ascendancy/Eminence': 'Yes'}
+                  'This is the PC I want counted for purposes '
+                  'of Ascendancy/Eminence': 'No'}
 
-        month9 = {'Timestamp': '9/19/14 12:43',
+        set2 = {'Timestamp': '9/19/14 12:43',
                  'What is your name?': 'Nintendo',
-                 'What is your Membership Number?': 'Hyrule9',
+                 'What is your Membership Number?': 'HYRULE9',
                  'Which PC will you be portraying today?': 'Zelda',
                  'Clan / Character Type': 'Princess',
                  'Covenant': 'Hyrulean',
                  'Cadence': '5',
                  'What is your City Status (Locals Only)': '5',
-                 'This is the PC I want counted for purposes'
+                 'This is the PC I want counted for purposes '
                  'of Ascendancy/Eminence': 'Yes'}
 
-        data = ena.data_to_dict(data)
-        self.assertEqual(ena.filter(data), data)
-        self.assertEqual(ena.filter(data, month=9), [month9])
+        data1 = ena.data_to_dict(data)
+        self.assertEqual(ena.filter_list(data1), data1)
+        self.assertEqual(ena.filter_list(data1, {'month': 9}), [set2])
 
-        self.assertEqual(ena.filter(data, month=7),
-                         [month7])
+        self.assertEqual(ena.filter_list(data1, {'month': 7}),
+                         [set1])
+
+        data2 = ena.data_to_dict(data)
+        self.assertEqual(ena.filter_list(data2), data2)
+        self.assertEqual(ena.filter_list(data2, {'year': 2013}), [set1])
+        self.assertEqual(ena.filter_list(data2, {'year': 2014}), [set2])
+
+        self.assertEqual(ena.filter_list(data2, {'year': 13}), [set1])
+        self.assertEqual(ena.filter_list(data2, {'year': 14}), [set2])
+
+        data3 = ena.data_to_dict(data)
+        self.assertEqual(ena.filter_list(data3), data3)
+        self.assertEqual(ena.filter_list(data3, {'clan': 'princess'}), [set2])
+        self.assertEqual(ena.filter_list(data3, {'covenant': 'hyrulean'}), [set2])
+
+        self.assertEqual(ena.filter_list(data3, {'clan': 'bullet'}), [set1])
+        self.assertEqual(ena.filter_list(data3, {'covenant': 'bowserite'}), [set1])
+
+        data4 = ena.data_to_dict(data)
+        self.assertEqual(ena.filter_list(data4), data4)
+
+        self.assertEqual(ena.filter_list(data4, {'count?': 'yes'}), [set2])
+        self.assertEqual(ena.filter_list(data4, {'count?': 'no'}), [set1])
+
+
+
+class ParseMonthTest(unittest.TestCase):
+    def test_parse_date(self):
+        values = (("1/12", {'month': 1, 'year': 12}),
+                  ("2/12", {'month': 2, 'year': 12}),
+                  ("2/14", {'month': 2, 'year': 14}),
+                 )
+        for date_string, result in values:
+            self.assertEqual(ena.parse_date(date_string), result)
+
+
+class TestStatusCalculation(unittest.TestCase):
+    def test_calculate_status(self):
+        data = [
+            ['Timestamp', 'What is your name?',
+             'What is your Membership Number?',
+             'Which PC will you be portraying today?',
+             'Clan / Character Type', 'Covenant', 'Cadence',
+             'What is your City Status (Locals Only)',
+             'This is the PC I want counted for purposes '
+             'of Ascendancy/Eminence'],
+            ['7/8/13 14:43', 'Nintendo2', 'MUSHROOMKINGDOM4', 'Bonzai Bill',
+             'Bullet', 'Bowserite', '1', '0 - None or Non Local', 'Yes'],
+            ['9/19/14 12:43', 'Nintendo1', 'HYRULE9', 'Zelda',
+             'Princess', 'Hyrulean', '5', '5', 'Yes'],
+            ['9/29/14 11:43', 'Nintendo1', 'HYRULE9', 'Zelda',
+             'Princess', 'Hyrulean', '5', '5', 'Yes']
+        ]
+
+        member_results = {'clan': {'bullet': 1, 'princess': 1},
+                          'covenant': {'bowserite': 1, 'hyrulean': 1}}
+
+        status_results = {'clan': {'bullet': 0, 'princess': 5},
+                          'covenant': {'bowserite': 0, 'hyrulean': 5}}
+
+        dict_data = ena.data_to_dict(data)
+        ena.STFU = True
+        members, statuses = ena.calculate_status(dict_data)
+        self.assertEqual(members, member_results)
+        self.assertEqual(statuses, status_results)
 
 
 if __name__ == '__main__':
